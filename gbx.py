204907c39848892fa0557f78f2e14682eaeec3f0
taizou <cactusgunman@gmail.com>
2020-09-06 23:15:35 +0100

    footer input & writing (not with actual footer data tho)

--- gbx.py
+++ gbx.py
@@ -2,6 +2,7 @@ import os
 import sys
 import hashlib
 import zlib
+import re
 
 gbxMajorVer = 1
 gbxMinorVer = 0
@@ -12,6 +13,7 @@ def bytesToInt(bytes):
 
 class FooterData:
 
+	size = None
 	majorVer = None
 	minorVer = None
 	supportedVer = False
@@ -35,6 +37,9 @@ class FooterData:
 		self.hasBattery = footer[4]
 		self.hasRtc = footer[5]
 		self.hasRumble = footer[6]
+		
+	def build(self):
+		return b"penispenispenispenispenispenispenispenispenispenispenispenispeni"
 		
 class RomLoader:
 
@@ -112,6 +117,81 @@ class RomManager:
 		except OSError:
 			print("\nError loading file")
 		print()	
+		
+	def setFooter(self):
+	
+		if (not self._romLoader.isGbx):
+			wasGbx = False
+			self._footerData = FooterData()
+			self._romLoader.isGbx = True
+			self._romLoader.romWithoutFooter = self._romLoader.fullRom
+		else:
+			wasGbx = True
+
+		doTheWrite = None
+		while (doTheWrite == None):
+			self._takeFooterFromInput(wasGbx)
+			writeInput = None
+			while (writeInput == None or writeInput.lower() not in ("y", "n", "x")):
+				writeInput = input("Is this OK? (Y) Save / (N) Re-enter / (X) Exit: ")
+			if (writeInput.lower() == "y"):
+				doTheWrite = True
+			elif (writeInput.lower() == "x"):
+				doTheWrite = False
+			
+		if (doTheWrite):
+			self._romLoader.gbxFooter = self._footerData.build()	
+			self._beSaving()
+		
+	def _takeFooterFromInput(self, wasGbx):
+			
+		self._footerData.size = 64
+		self._footerData.majorVer = gbxMajorVer
+		self._footerData.minorVer = gbxMinorVer
+		self._footerData.supportedVer = True
+		self._footerData.romSize = len(self._romLoader.romWithoutFooter)
+
+		editStr = self._buildEditStr(wasGbx, self._footerData.mapper)
+		newMapper = None
+		while (newMapper == None or (not wasGbx and newMapper == "") or len(newMapper) > 4):
+			newMapper = input(f"Enter new mapper code (max 4 chars){editStr}: ")
+		self._footerData.mapper = newMapper or self._footerData.mapper
+
+		editStr = self._buildEditStr(wasGbx, self._footerData.hasBattery)
+		newBatt = None
+		while (newBatt == None or (not wasGbx and newBatt == "") or (newBatt != "" and newBatt not in ("0", "1"))):
+			newBatt = input(f"Enter has battery 0=no 1=yes{editStr}: ")
+		self._footerData.hasBattery = int(newBatt) if newBatt else self._footerData.hasBattery
+
+		editStr = self._buildEditStr(wasGbx, self._footerData.hasRumble)
+		newRumble = None
+		while (newRumble == None or (not wasGbx and newRumble == "") or (newRumble != "" and newRumble not in ("0", "1"))):
+			newRumble = input(f"Enter has rumble 0=no 1=yes{editStr}: ")
+		self._footerData.hasRumble = int(newRumble) if newRumble else self._footerData.hasRumble
+
+		editStr = self._buildEditStr(wasGbx, self._footerData.hasRtc)
+		newRtc = None
+		while (newRtc == None or (not wasGbx and newRtc == "") or (newRtc != "" and newRtc not in ("0", "1"))):
+			newRtc = input(f"Enter has RTC 0=no 1=yes{editStr}: ")
+		self._footerData.hasRtc = int(newRtc) if newRtc else self._footerData.hasRtc
+
+		editStr = self._buildEditStr(wasGbx, self._footerData.ramSize)
+		newRamSize = None
+		while (newRamSize == None or (not wasGbx and newRamSize == "") or (newRamSize != "" and not re.search("^[0-9]+$", newRamSize))):
+			newRamSize = input(f"Enter RAM size in bytes{editStr}: ")
+		self._footerData.ramSize = int(newRamSize) if newRamSize else self._footerData.ramSize
+
+		print()
+		self._printFooter(self._footerData, "New GBX footer")
+		print()
+		
+
+	def _buildEditStr(self, wasGbx, initValue):
+		if (wasGbx):
+			return f", or leave blank to keep current ({initValue})"
+		else:
+			return ""
+		return extraStr	
 	
 	def removeFooter(self):
 		newFilename = self._getFilename()
@@ -119,11 +199,7 @@ class RomManager:
 		self._romLoader.isGbx = False
 		self._romLoader.romWithoutFooter = None
 		self._romLoader.gbxFooter = None
-		try:
-			self._romLoader.save(newFilename)
-			print(f"\nRemoved footer and saved as {newFilename}")
-		except OSError:
-			print("\nError saving file")
+		self._beSaving()
 		
 	def printAllHashes(self):
 		self.printHashes("File hashes", self._romLoader.fullRom)
@@ -131,52 +207,71 @@ class RomManager:
 			self.printHashes("ROM data hashes", self._romLoader.romWithoutFooter)
 			self.printHashes("Footer hashes", self._romLoader.gbxFooter)
 			
+	def _beSaving(self):
+		newFilename = self._getFilename()
+		try:
+			self._romLoader.save(newFilename)
+			print(f"\nSaved as {newFilename}")
+		except OSError:
+			print("\nError saving file")
+		
 	def _getFilename(self):
 		filename = None
 		while (not filename):
 			filename = input("Enter new filename (will overwrite if present): " )
 		return filename	
-	
-	def _parseFooter(self):
-		footerData = FooterData()
-		footerData.parse(self._romLoader.gbxFooter)
-		print(f"GBX footer found: ver {footerData.majorVer}.{footerData.minorVer}, size {footerData.size} bytes")	
+		
+	def _printFooter(self, footerData, prefix):
+		print(f"{prefix}: ver {footerData.majorVer}.{footerData.minorVer}, size {footerData.size} bytes")	
 		if (not footerData.supportedVer):
 			print("GBX version not supported!!")
 		else:
 			print(f"Mapper {footerData.mapper} / Batt {footerData.hasBattery} / Rumble {footerData.hasRumble} / RTC {footerData.hasRtc} / ROM {footerData.romSize} bytes / RAM {footerData.ramSize} bytes")
+	
+	def _parseFooter(self):
+		footerData = FooterData()
+		footerData.parse(self._romLoader.gbxFooter)
+		self._printFooter(footerData, "Found GBX footer")
 		return footerData
+		
+def run():
+	print(f"GBX ROM Tool v{gbxMajorVer}.{gbxMinorVer}.{scriptRevisionVer}")
+	if len(sys.argv) != 2:
+		print("Usage: py gbx.py filename.gbx")
+		exit()
 
-print(f"GBX ROM Tool v{gbxMajorVer}.{gbxMinorVer}.{scriptRevisionVer}")
-if len(sys.argv) != 2:
-	print("Usage: py gbx.py filename.gbx")
-	exit()
+	romManager = RomManager()
+	romManager.loadFile(sys.argv[1])
 
-romManager = RomManager()
-romManager.loadFile(sys.argv[1])
+	if (romManager.loadedGbx):
+		validOptions = ["h", "e", "r", "x"]
+		print("Options: (H) Display hashes / (E) Edit footer / (R) Remove footer / (X) Exit")
+	elif (romManager.loadedNonGbx):
+		validOptions = ["h", "a", "x"]
+		print("Options: (H) Display hashes / (A) Add footer / (X) Exit")
+	else:
+		exit()
 
-if (romManager.loadedGbx):
-	validOptions = ["h", "e", "r", "x"]
-	print("Options: (H) Display hashes / (E) Edit footer / (R) Remove footer / (X) Exit")
-elif (romManager.loadedNonGbx):
-	validOptions = ["h", "a", "x"]
-	print("Options: (H) Display hashes / (A) Add footer / (X) Exit")
-else:
-	exit()
+	mode = None
+	while (mode not in validOptions):
+		mode = input("Enter option: ").lower()
+	print()	
 
-mode = None
-while (mode not in validOptions):
-	mode = input("Enter option: ").lower()
-print()	
+	if (mode == "h"):
+		romManager.printAllHashes()
+	elif (mode == "a"):
+		romManager.setFooter()
+	elif (mode == "r"):
+		romManager.removeFooter()
+	elif (mode == "e"):
+		romManager.setFooter()
+	else:
+		print("Bye")
+	print()	
 
-if (mode == "h"):
-	romManager.printAllHashes()
-elif (mode == "a"):
-	print("add not done yet")
-elif (mode == "r"):
-	romManager.removeFooter()
-elif (mode == "e"):
-	print("edit not done yet")
-else:
-	print("Bye")
-print()	
+try:
+	run()
+except KeyboardInterrupt:
+	pass
+
+	
